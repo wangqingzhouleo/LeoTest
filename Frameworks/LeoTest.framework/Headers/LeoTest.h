06174843bb0ecd8b952f1d5cca35125aca1d52f1
leo.wang <leo.wang@massive.co>
2018-11-19 15:03:40 +1100

    v0.1.0

---
+++ Frameworks/LeoTest.framework/Headers/LeoTest.h
@@ -0,0 +1,19 @@
+//
+//  LeoTest.h
+//  LeoTest
+//
+//  Created by Leo Wang on 19/11/18.
+//  Copyright © 2018 Massive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeoTest.
+FOUNDATION_EXPORT double LeoTestVersionNumber;
+
+//! Project version string for LeoTest.
+FOUNDATION_EXPORT const unsigned char LeoTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeoTest/PublicHeader.h>
+
+
---
+++ LeoTest/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  LeoTest
+//
+//  Created by Leo Wang on 19/11/18.
+//  Copyright © 2018 Massive. All rights reserved.
+//
+
+import UIKit
+
+public class TestClass {
+
+    public static let version = "0.1.0"
+}
